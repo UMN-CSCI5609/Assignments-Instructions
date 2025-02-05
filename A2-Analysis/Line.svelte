2d62e45fc93ba1e5d1acc0b25ddda739f2192145
wangqianwen0418 <wangqianwen0418@163.com>
2025-02-05 10:45:28 -0600

    update A2 line chart instructions

--- A2-Analysis/Line.svelte
+++ A2-Analysis/Line.svelte
@@ -1,126 +1,124 @@
 <script lang="ts">
-    import * as d3 from "d3";
-    type TProps = {
-        data: Array<{ x: Date; y: number }>;
-        yearRange: [Date, Date] | undefined;
-        width?: number;
-        height?: number;
-    };
-    let {
-        data = [],
-        yearRange = $bindable(),
-        height = 150,
-        width = 600,
-    }: TProps = $props();
+  import * as d3 from "d3";
+  type TProps = {
+    data: Array<{ x: Date; y: number }>;
+    yearRange: [Date, Date] | undefined;
+    width?: number;
+    height?: number;
+  };
+  let {
+    data = [],
+    yearRange = $bindable(),
+    height = 150,
+    width = 600,
+  }: TProps = $props();
 
-    const margin = {
-        top: 15,
-        bottom: 50,
-        left: 30,
-        right: 10,
-    };
+  const margin = {
+    top: 15,
+    bottom: 50,
+    left: 30,
+    right: 10,
+  };
 
-    let usableArea = {
-        top: margin.top,
-        right: width - margin.right,
-        bottom: height - margin.bottom,
-        left: margin.left,
-    };
+  let usableArea = {
+    top: margin.top,
+    right: width - margin.right,
+    bottom: height - margin.bottom,
+    left: margin.left,
+  };
 
-    const xScale = $derived(
-        d3
-            .scaleTime()
-            .domain(d3.extent(data.map((d) => d.x)) as [Date, Date])
-            .range([usableArea.left, usableArea.right]),
-    );
+  const xScale = $derived(
+    d3
+      .scaleTime()
+      .domain(d3.extent(data.map((d) => d.x)) as [Date, Date])
+      .range([usableArea.left, usableArea.right])
+  );
 
-    const yScale = $derived(
-        d3
-            .scaleLinear()
-            .domain(d3.extent(data.map((d) => d.y)) as [number, number])
-            .range([usableArea.bottom, usableArea.top]),
-    );
+  const yScale = $derived(
+    d3
+      .scaleLinear()
+      .domain(d3.extent(data.map((d) => d.y)) as [number, number])
+      .range([usableArea.bottom, usableArea.top])
+  );
 
-    // tip2: this line generator will create a svg path from the data
-    // const lineGenerator = d3
-    //     .line()
-    //     .x((d) => xScale(d.x)) // Map x-coordinate
-    //     .y(xxx); // Map y-coordinate
+  // tip2: this line generator will create a svg path from the data
+  // const lineGenerator = d3
+  //     .line()
+  //     .x((d) => xScale(d.x)) // Map x-coordinate
+  //     .y(xxx); // Map y-coordinate
+  //     .curve(d3.curveBasis);
 
-    const path = $derived(lineGenerator(data));
+  const path = $derived(lineGenerator(data));
 
-    let xAxis: any = $state(),
-        yAxis: any = $state(),
-        brushElement: any = $state();
+  let xAxis: any = $state(),
+    yAxis: any = $state(),
+    brushElement: any = $state();
 
-    function updateAxis() {
-        if (!xScale || !yScale) {
-            return;
-        }
-        d3.select(xAxis).call(d3.axisBottom(xScale));
-
-        d3.select(yAxis).call(d3.axisLeft(yScale));
+  function updateAxis() {
+    if (!xScale || !yScale) {
+      return;
     }
+    d3.select(xAxis).call(d3.axisBottom(xScale));
 
-    function handleBrush(event: any) {
-        // tip3: this function will be called at Brush end, and we will use it to update the yearRange
-        // const selection = event.selection;
-        // if (selection) {
-        //     const [start, end] = selection.map(["which scale should we use here?"].invert); // Convert from pixel values to Date
-        //     yearRange = xxx
-        // } else {
-        //     yearRange = xxxx
-        // }
-    }
+    d3.select(yAxis).call(d3.axisLeft(yScale));
+  }
 
-    function setupBrush() {
-        const brush = d3
-            .brushX()
-            .extent([
-                [usableArea.left, usableArea.top],
-                [usableArea.right, usableArea.bottom],
-            ])
-            .on("brush end", handleBrush);
+  function handleBrush(event: any) {
+    // tip3: this function will be called at Brush end, and we will use it to update the yearRange
+    // const selection = event.selection;
+    // if (selection) {
+    //     const [start, end] = selection.map(["which scale should we use here?"].invert); // Convert from pixel values to Date
+    //     yearRange = xxx
+    // } else {
+    //     yearRange = xxxx
+    // }
+  }
 
-        d3.select(brushElement).call(brush);
-    }
+  function setupBrush() {
+    const brush = d3
+      .brushX()
+      .extent([
+        [usableArea.left, usableArea.top],
+        [usableArea.right, usableArea.bottom],
+      ])
+      .on("brush end", handleBrush);
 
-    // the $effect function is used to run a function whenever the reactive variables change, also known as a side effect
-    $effect(() => {
-        setupBrush();
-        updateAxis();
-    });
+    d3.select(brushElement).call(brush);
+  }
+
+  // the $effect function is used to run a function whenever the reactive variables change, also known as a side effect
+  $effect(() => {
+    setupBrush();
+    updateAxis();
+  });
 </script>
 
 <svg {width} {height} class="line">
-    <!-- tip2: add the line here -->
-    <!-- <path d={path} fill=xx stroke=xx stroke-width="1" /> -->
-    <g class="points">
+  <!-- tip2: add the line here, the circiles can help validate your curve -->
+  <!-- <path d={path} fill=xx stroke=xx stroke-width="1" /> -->
+  <!-- <g class="points">
         {#each data as point (point.x)}
             <circle
-                cx={xScale(point.x)}
-                cy={yScale(point.y)}
-                r="1.5"
-                fill="#222"
+                xxx
             />
         {/each}
-    </g>
-    <g transform="translate(0, {usableArea.bottom})" bind:this={xAxis} />
-    <g transform="translate({usableArea.left}, 0)" bind:this={yAxis} />
+    </g> -->
+  <g transform="translate(0, {usableArea.bottom})" bind:this={xAxis} />
+  <g transform="translate({usableArea.left}, 0)" bind:this={yAxis} />
 
-    <!-- tip3: add the brush here. -->
-    <!-- <g class="brush" bind:this={xxx} /> -->
+  <!-- tip3: add the brush here. -->
+  <!-- <g class="brush" bind:this={xxx} /> -->
 
-    <text x={width / 2} y={height - 5} text-anchor="middle">
-        Number of Movies by Year:
+  <text x={width / 2} y={height - 5} text-anchor="middle">
+    Number of Movies by Year:
+  </text>
+  {#if yearRange}
+    <text x={width / 2} y={height - 20} text-anchor="middle">
+      {yearRange[0].getFullYear()} - {yearRange[1].getFullYear()}
     </text>
-    {#if yearRange}
-        <text x={width / 2} y={height - 20} text-anchor="middle">
-            {yearRange[0].getFullYear()} - {yearRange[1].getFullYear()}
-        </text>
-    {:else}
-        <text x={width / 2} y={height - 20} text-anchor="middle">
-            Brush to select a range
-        </text>
-    {/if}
+  {:else}
+    <text x={width / 2} y={height - 20} text-anchor="middle">
+      Brush to select a range
+    </text>
+  {/if}
 </svg>
